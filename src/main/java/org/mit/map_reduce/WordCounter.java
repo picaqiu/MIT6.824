9f4f47250c3146dc3d36994a3df1b00640aaede6
picaqiu <673396844@qq.com>
2024-01-25 10:44:38 +0800

    #Add add word counter

---
+++ src/main/java/org/mit/map_reduce/WordCounter.java
@@ -0,0 +1,32 @@
+package org.mit.map_reduce;
+
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class WordCounter {
+    private Map<String, Long> data = new HashMap<>();
+
+
+    public void map(List<String> words) {
+        data = words.stream()
+                .map(String::toLowerCase)
+                .collect(Collectors.toMap(Function.identity(), word -> 1L, Long::sum));
+    }
+
+    public Map<String, Long> reduce() {
+        return data;
+    }
+
+    public static void main(String[] args) {
+        WordCounter wordCounter = new WordCounter();
+        String inputText = "Hello World Hello Java World";
+
+        // Tokenize the input text into words
+        List<String> words = Arrays.asList(inputText.split("\\s+"));
+        wordCounter.map(words);
+        wordCounter.reduce().forEach((k,v)->System.out.println(k+" : " + v));
+    }
+
+
+}
